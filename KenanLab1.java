adcc52c3b17dc54f0dbfadaa1c1ef504149d238d
KenanJones <kjones1303@volmail.jalc.edu>
2017-01-17 10:32:15 -0600

    Add files via upload

---
+++ KenanLab1.java
@@ -0,0 +1,15 @@
+import java.awt.event.*;
+
+public class KenanLab1 implements KeyListener
+{
+   public static void main (String[] args)
+   {
+   
+   }
+   public void keyPressed(KeyEvent e)
+   {
+   
+   }
+   public void keyTyped(KeyEvent e) {}
+   public void keyReleased(KeyEvent e) {}
+}
