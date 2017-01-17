1402a7ccb691c2b6b11790420121788562a09253
KenanJones <kjones1303@volmail.jalc.edu>
2017-01-17 11:40:54 -0600

    Add files via upload

--- KenanLab1.java
+++ KenanLab1.java
@@ -1,16 +1,10 @@
 import java.awt.event.*;
-import java.util.*;
 
 public class KenanLab1 implements KeyListener
 {
-private int[][] maze;
-private int xLoc, yLoc;
    public static void main (String[] args)
    {
-   maze = new int[8][16]
-   Random random = new Random();
-   xLoc = random.nextInt(16);
-   yLoc = random.nextInt(8);
+   
    }
    public void keyPressed(KeyEvent e)
    {
