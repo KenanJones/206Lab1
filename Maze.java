a8c598d5b67653f03ce9fe22f9c0f545bd87026f
KenanJones <kjones1303@volmail.jalc.edu>
2017-01-19 11:52:10 -0600

    Add files via upload

--- Maze.java
+++ Maze.java
@@ -26,32 +26,45 @@ private static boolean finished = false;
    history = new ArrayList<String>();
 
    try{
-      File file1 = new File("maze1.txt");
-      Scanner inputFile = new Scanner(file1);
+      File file = new File(getFileInput());
+      Scanner inputFile = new Scanner(file);
       maze = new int[inputFile.nextInt()][inputFile.nextInt()];
+      inputFile.nextLine();//consumes an empty line left by nextInt
+      System.out.println(maze.length);
+      System.out.println(maze[0].length);
       int row = 0;
       while(inputFile.hasNext()){
          String input = inputFile.nextLine();
          for(int i = 0; i < input.length(); i++){
-           maze[row][i] = input.charAt(i);
-           i++;
+            if(input.charAt(i) == 'x')
+               maze[row][i] = 1;
+            else if(input.charAt(i) == 'f')
+               maze[row][i] = -1;
          }
+      row++;
       }
    }catch(IOException e){
       System.out.println("invalid maze file");
       System.exit(0);
    }
 
-   for(int col = 0; col < maze.length; col++){
-      for(int row = 0; row < maze[col].length; row++){
+   for(int row = 0; row < maze.length; row++){
+      for(int col = 0; col < maze[row].length; col++){
          //replace with input file info. (this is the border)
-         if(col==0 || col == maze.length-1 || row == 0 || row == maze[col].length-1)
-            maze[col][row] = 1;
-         }
+         //if(col==0 || col == maze.length-1 || row == 0 || row == maze[col].length-1)
+            //maze[col][row] = 1;
+         //System.out.print(maze[row][col]);
       }
+   //System.out.println();
+   }
    Random random = new Random();
-   xLoc = random.nextInt(12)+ 2;
-   yLoc = random.nextInt(4) + 2;
+   boolean test = true;
+   while(test){
+      yLoc = random.nextInt(maze.length);
+      xLoc = random.nextInt(maze[0].length);
+   if(maze[yLoc][xLoc] == 0) test = false;
+   }
+   System.out.println(xLoc + ", " + yLoc);
    options = "";
    frame.setVisible(true);
    showOptions();
@@ -71,15 +84,14 @@ private static boolean finished = false;
 
    public static void main (String[] args) throws IOException{
    new Maze();
-   if(finished)
-      System.out.println("Congratulations!\nYou Survived!");
    }
    //tests whether a given move is valid, and changes your position if it is.
    public void move(int x, int y){
    if(x == -1 && options.contains("left")){xLoc--; history.add("left");}
    if(x == 1 && options.contains("right")){xLoc++; history.add("right");}
-   if(y == -1 && options.contains("up"))  {xLoc--; history.add("up");}
-   if(y == 1 && options.contains("down")) {xLoc++; history.add("down");}
+   if(y == -1 && options.contains("up"))  {yLoc--; history.add("up");}
+   if(y == 1 && options.contains("down")) {yLoc++; history.add("down");}
+   if(maze[yLoc][xLoc] == -1){System.out.println("Congratulations!\nYou Escaped!");System.exit(0);}
    showOptions();
    }
    //fills the labels with the correct text
@@ -92,10 +104,35 @@ private static boolean finished = false;
    //adds the apropriate word if the direction is open
    public void getOptions(){
    options = "";
-   if(maze[xLoc-1][yLoc]==0)options += "left, ";
-   if(maze[xLoc+1][yLoc]==0)options += "right, ";
-   if(maze[xLoc][yLoc-1]==0)options += "up, ";
-   if(maze[xLoc][yLoc+1]==0)options += "down, ";
+   if(maze[yLoc-1][xLoc]!=1)options += "up, ";
+   if(maze[yLoc+1][xLoc]!=1)options += "down, ";
+   if(maze[yLoc][xLoc-1]!=1)options += "left, ";
+   if(maze[yLoc][xLoc+1]!=1)options += "right, ";
+   }
+   
+   public String getFileInput(){
+   String filename = "";
+   boolean test = true;
+   while(test){
+      filename = JOptionPane.showInputDialog("please enter a maze file name.");
+      if(filename == null)filename = "maze1.txt";
+      try{
+         System.out.println("try1");
+         File testFile = new File(filename);
+         Scanner testScan = new Scanner(testFile);
+         test = false;
+      }
+      catch(IOException e){
+         try{
+            File testFile = new File(filename + ".txt");
+            Scanner testScan = new Scanner(testFile);
+            test = false;
+         }
+         catch(IOException ex){
+         }
+      }
+   }
+   return filename;
    }
 
    public void keyPressed(KeyEvent e){
