ebd7945b91c93d5d83978ac092839b7613e26673
KenanJones <kjones1303@volmail.jalc.edu>
2017-02-08 16:17:52 -0600

    update Maze.java
    uses command line arguments

--- Maze.java
+++ Maze.java
@@ -1,148 +1,347 @@
 import java.awt.event.*;
-import java.awt.*;
+
 import java.util.*;
+
 import javax.swing.*;
+
 import java.io.*;
 
-public class Maze implements KeyListener{
+
+
+public class Maze2 implements KeyListener{
+
+   
+
    private JFrame frame;
-   private JPanel panel;
+
    private JTextField field;
-   private JLabel label;
-   private JTextArea textArea;
-   
+
+
+
    private int[][] maze;
+
    private int xLoc, yLoc;
-   private ArrayList<String> history;
-   private String options;
-   
+
+   private ArrayList<String> history, options;
+
+
+
    public Maze(){
-      frame = new JFrame();
-      frame.setSize(200,200);
-      frame.setLocation(500,300);
-      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      buildPanel();
-   
-      history = new ArrayList<String>();
-   
-      try{
-         File file = new File(getFileInput());
-         Scanner inputFile = new Scanner(file);
-         maze = new int[inputFile.nextInt()][inputFile.nextInt()];
-         inputFile.nextLine();//consumes an empty line left by nextInt
-         int row = 0;
-         while(inputFile.hasNext()){
-            String input = inputFile.nextLine();
-            for(int i = 0; i < input.length(); i++){
-               if(input.charAt(i) == 'x')
-                  maze[row][i] = 1;
-               else if(input.charAt(i) == 'f')
-                  maze[row][i] = -1;
-            }
-         row++;
-         }
-      }catch(IOException e){
-         System.out.println("invalid maze file");
-         System.exit(1);
-      }
-   
-      Random random = new Random();
-      boolean test = true;
-      while(test){
-         yLoc = random.nextInt(maze.length);
-         xLoc = random.nextInt(maze[0].length);
-      if(maze[yLoc][xLoc] == 0) test = false;
-      }
-      System.out.println(xLoc + ", " + yLoc);
-      options = "";
-      frame.setVisible(true);
-      showOptions();
-      }
-   
-   public void buildPanel(){
-      panel = new JPanel();
-      panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
-      field = new JTextField();
-      field.addKeyListener(this);
-      //field.setVisible(false);
-      field.setMaximumSize(new Dimension(1,1));
-      panel.add(field);
-      label = new JLabel();
-      label.setAlignmentX(0.5f);
-      panel.add(label);
-      textArea = new JTextArea(5, 20); 
-      textArea.setEditable(false);
-      textArea.setLineWrap(true);
-      textArea.setWrapStyleWord(true);
-      panel.add(textArea);
-      frame.add(panel);
-      }
-   
+
+      
+
+      getMazeFile("maze1.txt");
+
+      buildDisplay();
+
+      startGame();
+
+   }
+
+public Maze(String mazeFileName){
+
+      
+
+      getMazeFile(mazeFileName);
+
+      buildDisplay();
+
+      startGame();
+
+   }
+
+public Maze(String mazeFileName, int monsters){
+
+      
+
+      getMazeFile(mazeFileName);
+
+      buildDisplay();
+
+      startGame();
+
+   }
+
+
+
    public static void main (String[] args) throws IOException{
+
       new Maze();
+
       }
-      
-      public void move(int x, int y){
+
+
+
+   public void buildDisplay(){
+
+      frame = new JFrame();
+
+      frame.setSize(300,15);
+
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+      field = new JTextField();
+
+      field.addKeyListener(this);
+
+      frame.add(field);
+
+   }
+
+
+
+   public void move(int x, int y){
+
       if(x == -1 && options.contains("left")){xLoc--; history.add("left");}
-      if(x == 1 && options.contains("right")){xLoc++; history.add("right");}
-      if(y == -1 && options.contains("up"))  {yLoc--; history.add("up");}
-      if(y == 1 && options.contains("down")) {yLoc++; history.add("down");}
+
+      else if(x == 1 && options.contains("right")){xLoc++; history.add("right");}
+
+      else if(y == -1 && options.contains("up"))  {yLoc--; history.add("up");}
+
+      else if(y == 1 && options.contains("down")) {yLoc++; history.add("down");}
+
+      else{System.out.print("invalid move! ");}
+
       if(maze[yLoc][xLoc] == -1){
+
          System.out.printf("Congratulations! You Escaped in %d moves!\n",history.size());
+
          System.out.println("You Moved: " + history);
-         System.exit(0);}
+
+         System.exit(0);
+
+      }
+
       showOptions();
+
+   }
+
+
+
+   public void getMazeFile(String filename){
+
+      try{
+
+         testFile(filename);
+
+         buildMaze(filename);
+
       }
-      
-   public void showOptions(){
-      getOptions();
-      label.setText(options);
-      textArea.setText("History: " + history);
-      }
-      
-   public void getOptions(){
-      options = "";
-      if(maze[yLoc-1][xLoc]!=1)options += "up, ";
-      if(maze[yLoc+1][xLoc]!=1)options += "down, ";
-      if(maze[yLoc][xLoc-1]!=1)options += "left, ";
-      if(maze[yLoc][xLoc+1]!=1)options += "right, ";
-      }
-      
-   public String getFileInput(){
-      String filename = "";
-      boolean test = true;
-      while(test){
-         filename = JOptionPane.showInputDialog("please enter a maze file name.");
-         if(filename == null)filename = "maze1.txt";
+
+      catch(IOException e){
+
          try{
-            File testFile = new File(filename);
-            Scanner testScan = new Scanner(testFile);
-            test = false;
+
+            testFile(filename += ".txt");
+
+            buildMaze(filename);
+
          }
-         catch(IOException e){
-            try{
-               filename += ".txt";
-               File testFile = new File(filename);
-               Scanner testScan = new Scanner(testFile);
-               test = false;
-            }
-            catch(IOException ex){
-            }
+
+         catch(IOException ex){
+
+	    System.out.println("invalid maze file");
+
+            System.exit(1);
+
          }
+
       }
-      return filename;
-      }
+
+   }
+
+
+
+   public void buildMaze(String filename){
+
+	 File file = new File(filename);
+
+         Scanner inputFile = new Scanner(file);
+	 
+	 maze = new int[inputFile.nextInt()][inputFile.nextInt()];
+
+         inputFile.nextLine();//consumes an empty line left by nextInt
+
+         int row = 0;
+
+         while(inputFile.hasNext()){
+
+            String input = inputFile.nextLine();
+
+            for(int i = 0; i < input.length(); i++){
+
+               if(input.charAt(i) == 'x')
+
+                  maze[row][i] = 1;
+
+               else if(input.charAt(i) == 'f')
+
+                  maze[row][i] = -1;
+
+            }
+
+         row++;
+
+         }
+
+         inputFile.close();
+
    
-   public void keyPressed(KeyEvent e){
-      //System.out.println(e.getKeyCode());
-      switch(e.getKeyCode())
-         {
-         case 37:move(-1,0);break;
-         case 38:move(0,-1);break;
-         case 39:move(1,0);break;
-         case 40:move(0,1);break;
+
+    /*public String getFileInput(){
+
+      String filename = "";
+
+      Scanner keyboard = new Scanner(System.in);
+
+      boolean test = true;
+
+      while(test){
+
+         System.out.print("please enter a maze file name.");
+
+         filename = keyboard.nextLine();
+
+         if(filename.equals(null))filename = "maze1.txt";
+
+         try{
+
+            testFile(filename);
+
+            test = false;
+
          }
+
+         catch(IOException e){
+
+            try{
+
+               testFile(filename += ".txt");
+
+               test = false;
+
+            }
+
+            catch(IOException ex){
+
+            }
+
+         }
+
       }
+
+      return filename;
+
+   }*/
+
+   
+
+   public void startGame(){
+
+      history = new ArrayList<String>();
+
+      options = new ArrayList<String>();
+
+      Random random = new Random();
+
+      boolean test = true;
+
+      while(test){
+
+         yLoc = random.nextInt(maze.length);
+
+         xLoc = random.nextInt(maze[0].length);
+
+         if(maze[yLoc][xLoc] == 0){
+
+            test = false;
+
+         }
+
+      }
+
+      System.out.println(xLoc + ", " + yLoc);
+
+      frame.setVisible(true);
+
+      showOptions();
+
+   }
+
+   
+
+   public void showOptions(){
+
+      getOptions();
+
+      System.out.println("Options: " + options);
+
+      field.grabFocus();
+
+   }
+
+
+
+   public void showHistory(){
+
+      System.out.println("Move History: " + history);
+
+      showOptions();
+
+   }
+
+
+
+   public void getOptions(){
+
+      options.clear();
+
+      if(maze[yLoc-1][xLoc]!=1)options.add("up");
+
+      if(maze[yLoc+1][xLoc]!=1)options.add("down");
+
+      if(maze[yLoc][xLoc-1]!=1)options.add("left");
+
+      if(maze[yLoc][xLoc+1]!=1)options.add("right");
+
+   }
+
+   
+
+   public void testFile(String filename) throws IOException{
+
+      File testFile = new File(filename);
+
+      Scanner testScan = new Scanner(testFile);
+
+   }
+
+
+
+   public void keyPressed(KeyEvent e){
+
+      //System.out.println(e.getKeyCode());
+
+      switch(e.getKeyCode())
+
+         {
+
+         case 37: move(-1,0);break;
+
+         case 38: move(0,-1);break;
+
+         case 39: move(1,0);break;
+
+         case 40: move(0,1);break;
+
+         default: showHistory();
+
+       }
+
+   }
+
    public void keyTyped(KeyEvent e) {}
+
    public void keyReleased(KeyEvent e) {}
+
 }
